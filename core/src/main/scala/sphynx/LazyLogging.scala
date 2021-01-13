270a2cbbb3d32ba9672c734d304ec0b45416af88
Kai Matsuda <matsudaskai@gmail.com>
2021-01-12 23:54:06 -0500

    Oss10 cd bug (#4)
    * OSS-10 debug

--- core/src/main/scala/sphynx/LazyLogging.scala
+++ core/src/main/scala/sphynx/LazyLogging.scala
@@ -4,8 +4,6 @@ import cats.effect.Sync
 import org.slf4j.{LoggerFactory, Logger => Underlying}
 
 trait LazyLogging[F[_]] {
-  private lazy val underlying: Underlying =
-    LoggerFactory.getLogger(getClass.getName)
-  protected def logger(implicit sync: Sync[F]): LazyLogger[F] =
-    LazyLogger(underlying)
+  private lazy val underlying: Underlying = LoggerFactory.getLogger(getClass.getName)
+  protected def logger(implicit sync: Sync[F]): LazyLogger[F] = LazyLogger(underlying)
 }
